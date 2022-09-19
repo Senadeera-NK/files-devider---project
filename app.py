8bc62179fbf907f7a6dfcf99c0c16847ad507b67
Senadeera NK <naduni.20191206@iit.ac.lk>
2022-09-19 11:52:36 +0530

    2o22/9/19

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
+@app.route("/", methods=["POST"])
+def upload():
   return render_template("start page.html")
-  

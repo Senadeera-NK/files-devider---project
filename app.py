10f9c2bd2c24856d459540a7a9a9f3fe32493984
Senadeera NK <naduni.20191206@iit.ac.lk>
2022-09-19 12:05:37 +0530

    2022/9/19 - 3

--- app.py
+++ app.py
@@ -2,6 +2,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/", methods=["POST"])
-def upload():
+@app.route("/")
+def index():
   return render_template("start page.html")
+
+if __name__ == "__main__":
+  from waitress import serve
+  serve(app, host="0.0.0.0", port=8080)
+
+def upload():
+  return render_template("uploading.html")

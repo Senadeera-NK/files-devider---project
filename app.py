b73e8fe2ae857b5d20e8ae92d7b461c29acb0a3c
Senadeera NK <naduni.20191206@iit.ac.lk>
2022-09-19 18:59:22 +0530

    2022/9/19 - 9

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from urllib import request
 from flask import Flask, render_template, request
+from flask_wtf import FlaskForm
+from wtforms import FileField
 
 app = Flask(__name__)
 
@@ -8,6 +10,14 @@ app = Flask(__name__)
 def index():
   return render_template("index.html")
 
-@app.route("/greet")
-def greet():
-  return render_template("greet.html", name=request.args.get("name","world"))
+@app.route("/upload")
+def upload():
+
+  return render_template("uploading.html")
+
+# @app.route("/greet")
+# def greet():
+#   return render_template("greet.html", name=request.args.get("name","world"))
+
+if __name__ == "__main__":
+  app.run(debug=True)

19f4b6050f4493018289daa1433d7d766c683c03
Senadeera NK <naduni.20191206@iit.ac.lk>
2022-09-19 16:53:06 +0530

    2022/9/19 - 7

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-from flask import Flask
+from urllib import request
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
+#default page's respond
 def index():
-  return "Helloo"
+  return render_template("index.html")
+
+@app.route("/greet")
+def greet():
+  return render_template("greet.html", name=request.args.get("name","world"))

2c7eaca3b55718c1ebbe808d26888f65b9b410f9
Senadeera NK <naduni.20191206@iit.ac.lk>
2022-09-20 12:51:23 +0530

    2022/9/20

--- app.py
+++ app.py
@@ -1,19 +1,31 @@
 from urllib import request
 from flask import Flask, render_template, request
 from flask_wtf import FlaskForm
-from wtforms import FileField
+from wtforms import FileField, SubmitField
+from werkzeug.utils import secure_filename
+import os
+# for please select the file for "upload" button
+from wtforms.validators import InputRequired
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = 'supersecretkey'
+app.config['UPLOAD_FOLDER'] = 'static/files'
 
-@app.route("/")
-#default page's respond
-def index():
-  return render_template("index.html")
+class UploadFileForm(FlaskForm):
+  file = FileField("File", validators=[InputRequired()])
+  submit = SubmitField("Upload File")
 
-@app.route("/upload")
+@app.route("/", methods=["GET","POST"])
+
+@app.route("/upload", methods=["GET","POST"])
 def upload():
-
-  return render_template("uploading.html")
+  form = UploadFileForm()
+  if form.validate_on_submit():
+    file = form.file.data #first grab the file
+    file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename(file.filename))) # then save the file
+    return 'File has been uploaded'
+  
+  return render_template("upload.html", form=form)
 
 # @app.route("/greet")
 # def greet():

5264cab424046670b595b34e4fbe6291f8d7d132
Senadeera NK <naduni.20191206@iit.ac.lk>
2022-09-27 17:30:32 +0530

    2022/9/27

--- app.py
+++ app.py
@@ -1,34 +1,29 @@
-from urllib import request
-from flask import Flask, render_template, request
-from flask_wtf import FlaskForm
-from wtforms import FileField, SubmitField
+from flask import *
 from werkzeug.utils import secure_filename
 import os
-# for please select the file for "upload" button
-from wtforms.validators import InputRequired
+
+UPLOAD_FOLDER = 'static/files'
+ALLOWED_EXTENSIONS = ['txt', 'pdf', 'docs', 'docx']
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = 'supersecretkey'
-app.config['UPLOAD_FOLDER'] = 'static/files'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['ALLOWED_EXTENSIONS'] = ALLOWED_EXTENSIONS
 
-class UploadFileForm(FlaskForm):
-  file = FileField("File", validators=[InputRequired()])
-  submit = SubmitField("Upload File")
-
-@app.route("/", methods=["GET","POST"])
-
-@app.route("/upload", methods=["GET","POST"])
+@app.route('/')
 def upload():
-  form = UploadFileForm()
-  if form.validate_on_submit():
-    file = form.file.data #first grab the file
-    file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename(file.filename))) # then save the file
-    return 'File has been uploaded'
-  return render_template("upload.html", form=form)
+  return render_template('upload.html')
 
-# @app.route("/greet")
-# def greet():
-#   return render_template("greet.html", name=request.args.get("name","world"))
+@app.route('/upload result', methods=['GET', 'POST'])
+def upload_result():
+  if request.method == 'POST':
+    f = request.files['file']
+    filename = secure_filename(f.filename)
+    f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    result = 'files uploaded successfully !!!'
+    return render_template('upload result.html', result=result)
+  result = "files failed to upload"
+  return render_template('upload result.html', result=result)
+
 
 if __name__ == "__main__":
   app.run(debug=True)
